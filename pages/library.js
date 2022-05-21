076da442837ecad521217391c64a5c6d9f7c92b7
manshad <manshadchangampalli010@gmail.com>
2022-05-21 12:27:27 +0530

    #added: the search bar function also added on the library page

--- pages/library.js
+++ pages/library.js
@@ -5,46 +5,84 @@ import {
   inputBox,
   searchIcon,
   latestItem,
-  itemBox,
+  resultBox,
 } from "../styles/Library.module.css";
 import SearchIcon from "../public/images/navImages/searchIcon.png";
 import Logo from "../Components/logo/Logo";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import LibraryItem from "../Components/libraryitem/LibraryItem";
 import Link from "next/link";
 
-export const getStaticProps = async() => {
-  const Response =await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}library/getrecentlyaddedlibraryName`)
-  const data = await Response.json()
+export const getStaticProps = async () => {
+  const Response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}library/getrecentlyaddedlibraryName`
+  );
+  const data = await Response.json();
 
   return {
-    props:{
-      data:data
-    }
-  }
+    props: {
+      data: data,
+    },
+  };
+};
 
-}
-
-
-const Library = ({data}) => {
-  const [pointTop, setPointTop] = useState(500);
-  const [pointLeft, setPointLeft] = useState(500);
+const Library = ({ data }) => {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchData, setSearchData] = useState([]);
+  const [windowClick, setWindowOnClick] = useState(false);
+  const allItemsRef = useRef();
+  const serachBoxRef = useRef();
+  const serachItemRef = useRef();
   useEffect(() => {
-    window.addEventListener("mousemove", (e) => {
-      setPointLeft(e.clientX);
-      setPointTop(e.clientY);
-    });
-  }, []);
+    if (allItemsRef.current) {
+      allItemsRef.current.addEventListener("click", (e) => {
+        if (serachBoxRef.current && serachItemRef.current) {
+          if (
+            !serachBoxRef.current.contains(e.target) &&
+            !serachItemRef.current.contains(e.target)
+          ) {
+            setWindowOnClick(false);
+          }
+        }
+      });
+    }
+  }, [windowClick]);
+
+  const handleOnChange = (e) => {
+    setWindowOnClick(true);
+    setSearchInput(e.target.value);
+    fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}library/search?name=${e.target.value}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setSearchData(data);
+      });
+  };
   return (
-    <div className={libraryPage}>
+    <div ref={allItemsRef} className={libraryPage}>
       <Logo />
       <NavBar />;
       <div className={inputBox}>
-        <input type="text" />
+        <input ref={serachBoxRef} onChange={handleOnChange} type="text" />
         <div className={searchIcon}>
           <Image width={20} height={20} alt="" src={SearchIcon} />
         </div>
+        {(searchData.length > 0 || searchInput.length > 0) && windowClick && (
+          <ul ref={serachItemRef} className={resultBox}>
+            {searchData.map((data, i) => (
+              <>
+                <Link href={`/library/${data._id}`} passHref>
+                  <li key={i}>{data.name}</li>
+                </Link>
+              </>
+            ))}
+            {searchData.length === 0 && searchInput.length > 0 && (
+              <li>No item found</li>
+            )}
+          </ul>
+        )}
       </div>
       <div className={latestItem}>
         {data.map((data, i) => {
